4aa0fc6c19566754d3f45624738460f66eef8b14
Zoran Regvart <zregvart+github@gmail.com>
2012-01-29 23:23:53 +0100

    preparation for relase on jenkins-ci pom.xml cleanup added me to Copyright holder in license blobs

--- src/main/java/org/jenkinsci/plugins/xvfb/XvfbBuildWrapper.java
+++ src/main/java/org/jenkinsci/plugins/xvfb/XvfbBuildWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 <whome?> All rights reserved.
+ * Copyright (c) 2012 Zoran Regvart All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
